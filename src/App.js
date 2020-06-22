070ee2a55f729e22e811e9089f965e38ea3dbacb
George Colon <gcolon021@gmail.com>
2020-06-22 13:46:23 -0400

    Removed commented code

--- src/App.js
+++ src/App.js
@@ -24,21 +24,3 @@ function App() {
 }
 
 export default App;
-
-
-// import FormControl from '@material-ui/core/FormControl';
-// import Select from '@material-ui/core/Select';
-// import InputLabel from '@material-ui/core/InputLabel';
-// import MenuItem from '@material-ui/core/MenuItem';
-// import Menu from "@material-ui/core/Menu"
-
-
-{/* <Menu>
-              <MenuItem>Date Created (oldest first)</MenuItem>
-              <MenuItem>Date Created (newest first)</MenuItem>
-              <MenuItem>Date Updated (oldest first)</MenuItem>
-              <MenuItem>Date Updated (newest first)</MenuItem>
-              <MenuItem>Title (ascending)</MenuItem>
-              <MenuItem>Title (descending)</MenuItem>
-              <MenuItem>By Relevance</MenuItem>
-            </Menu> */}
