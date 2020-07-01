cdc81972042193007dbc32ee3b44a6723aae0e78
gcolon021 <gcolon021@gmail.com>
2020-07-01 14:58:39 -0400

    first commit

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -10,3 +10,10 @@ const index = () => {
 }
 
 export default index
+
+// backgroundColor: "#fff",
+// width: "100%",
+// height: "100vh",
+// overflow: "hidden",
+// borderLeft: "1px solid #ececec",
+// position: "relative"
